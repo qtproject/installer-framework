dfb16001d52da19931d893fa0e09ccdacfc4739d
hjk <qthjk@ovi.com>
2011-11-19 13:05:13 +0100

    Use arrays for string literals.
    Change-Id: Idffb39db93784bf7f9029afa56509de59f81da53
    Reviewed-by: Tim Jenssen <tim.jenssen@nokia.com>

--- installerbuilder/libinstaller/qtcreator_constants.h
+++ installerbuilder/libinstaller/qtcreator_constants.h
@@ -1,46 +1,45 @@
 #ifndef QTCREATOR_CONSTANTS_H
 #define QTCREATOR_CONSTANTS_H
 
-#if defined ( Q_OS_MAC )
-    static const char *const QtCreatorSettingsSuffixPath =
+#if defined(Q_OS_MAC)
+    static const char QtCreatorSettingsSuffixPath[] =
         "/Qt Creator.app/Contents/Resources/Nokia/QtCreator.ini";
 #else
-    static const char *const QtCreatorSettingsSuffixPath =
+    static const char QtCreatorSettingsSuffixPath[] =
         "/QtCreator/share/qtcreator/Nokia/QtCreator.ini";
 #endif
 
-#if defined ( Q_OS_MAC )
-    static const char *const ToolChainSettingsSuffixPath =
+#if defined(Q_OS_MAC)
+    static const char ToolChainSettingsSuffixPath[] =
         "/Qt Creator.app/Contents/Resources/Nokia/toolChains.xml";
 #else
-    static const char *const ToolChainSettingsSuffixPath =
+    static const char ToolChainSettingsSuffixPath[] =
         "/QtCreator/share/qtcreator/Nokia/toolChains.xml";
 #endif
 
-#if defined ( Q_OS_MAC )
-    static const char *const QtVersionSettingsSuffixPath =
+#if defined(Q_OS_MAC)
+    static const char QtVersionSettingsSuffixPath[] =
         "/Qt Creator.app/Contents/Resources/Nokia/qtversion.xml";
 #else
-    static const char *const QtVersionSettingsSuffixPath =
+    static const char QtVersionSettingsSuffixPath[] =
         "/QtCreator/share/qtcreator/Nokia/qtversion.xml";
 #endif
 
-    //Begin - copied from Creator src\plugins\projectexplorer\toolchainmanager.cpp
-static const char *const TOOLCHAIN_DATA_KEY = "ToolChain.";
-static const char *const TOOLCHAIN_COUNT_KEY = "ToolChain.Count";
-static const char *const TOOLCHAIN_FILE_VERSION_KEY = "Version";
-static const char *const DEFAULT_DEBUGGER_COUNT_KEY = "DefaultDebugger.Count";
-static const char *const DEFAULT_DEBUGGER_ABI_KEY = "DefaultDebugger.Abi.";
-static const char *const DEFAULT_DEBUGGER_PATH_KEY = "DefaultDebugger.Path.";
+// Begin - copied from Creator src\plugins\projectexplorer\toolchainmanager.cpp
+static const char TOOLCHAIN_DATA_KEY[] = "ToolChain.";
+static const char TOOLCHAIN_COUNT_KEY[] = "ToolChain.Count";
+static const char TOOLCHAIN_FILE_VERSION_KEY[] = "Version";
+static const char DEFAULT_DEBUGGER_COUNT_KEY[] = "DefaultDebugger.Count";
+static const char DEFAULT_DEBUGGER_ABI_KEY[] = "DefaultDebugger.Abi.";
+static const char DEFAULT_DEBUGGER_PATH_KEY[] = "DefaultDebugger.Path.";
 
-static const char *const ID_KEY = "ProjectExplorer.ToolChain.Id";
-static const char *const DISPLAY_NAME_KEY = "ProjectExplorer.ToolChain.DisplayName";
-//End - copied from Creator
+static const char ID_KEY[] = "ProjectExplorer.ToolChain.Id";
+static const char DISPLAY_NAME_KEY[] = "ProjectExplorer.ToolChain.DisplayName";
+// End - copied from Creator
 
-//Begin - copied from Creator src\plugins\qt4projectmanager\qtversionmanager.cpp
-static const char *const QtVersionsSectionName = "QtVersions";
-static const char *const newQtVersionsKey = "NewQtVersions";
-//End - copied from Creator
+// Begin - copied from Creator src\plugins\qt4projectmanager\qtversionmanager.cpp
+static const char QtVersionsSectionName[] = "QtVersions";
+static const char newQtVersionsKey[] = "NewQtVersions";
+// End - copied from Creator
 
-
-#endif //QTCREATOR_CONSTANTS_H
+#endif // QTCREATOR_CONSTANTS_H
