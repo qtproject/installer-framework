91e4ca90408becb74e48b90c8105fd250201b6af
Niels Weber <niels.2.weber@nokia.com>
2012-02-06 09:46:16 +0100

    some more license header fixes
    Change-Id: I05a9a3fabd35638d37ce4e7c3dee2d21d5f73c78
    Reviewed-by: Tim Jenssen <tim.jenssen@nokia.com>

--- installerbuilder/libinstaller/qprocesswrapper.h
+++ installerbuilder/libinstaller/qprocesswrapper.h
@@ -1,10 +1,11 @@
 /**************************************************************************
 **
-** This file is part of Installer Framework**
+** This file is part of Installer Framework
 **
-** Copyright (c) 2010 Nokia Corporation and/or its subsidiary(-ies).*
+** Copyright (c) 2010-2012 Nokia Corporation and/or its subsidiary(-ies).
 **
-** Contact:  Nokia Corporation qt-info@nokia.com**
+** Contact: Nokia Corporation (info@qt.nokia.com)
+**
 **
 ** GNU Lesser General Public License Usage
 **
@@ -16,13 +17,19 @@
 ** http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
 **
 ** In addition, as a special exception, Nokia gives you certain additional
-** rights. These rights are described in the Nokia Qt LGPL Exception version
-** 1.1, included in the file LGPL_EXCEPTION.txt in this package.
+** rights. These rights are described in the Nokia Qt LGPL Exception
+** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
 **
-** If you are unsure which license is appropriate for your use, please contact
-** (qt-info@nokia.com).
+** Other Usage
+**
+** Alternatively, this file may be used in accordance with the terms and
+** conditions contained in a signed written agreement between you and Nokia.
+**
+** If you have questions regarding the use of this file, please contact
+** Nokia at info@qt.nokia.com.
 **
 **************************************************************************/
+
 #ifndef QPROCESSWRAPPER_H
 #define QPROCESSWRAPPER_H
 
