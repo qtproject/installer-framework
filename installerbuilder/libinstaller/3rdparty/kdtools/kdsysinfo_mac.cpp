1794e60452440968ff2bf311620ec52f3a22b4b3
kh1 <karsten.heimrich@nokia.com>
2012-02-22 10:21:44 +0100

    Implement missing running processes function.
    Change-Id: I0c90d2c347c88b42cf8976f635ec579be3f8f843
    Reviewed-by: Niels Weber <niels.2.weber@nokia.com>
    Reviewed-by: Tim Jenssen <tim.jenssen@nokia.com>

--- installerbuilder/libinstaller/3rdparty/kdtools/kdsysinfo_mac.cpp
+++ installerbuilder/libinstaller/3rdparty/kdtools/kdsysinfo_mac.cpp
@@ -25,6 +25,8 @@
 #include <Carbon/Carbon.h>
 
 #include <sys/mount.h>
+#include <sys/sysctl.h>
+#include <sys/types.h>
 
 #include <QtCore/QList>
 
@@ -69,7 +71,64 @@ QList<VolumeInfo> mountedVolumes()
 
 QList<ProcessInfo> runningProcesses()
 {
-    return QList<ProcessInfo>();
+    int mib[4] = {
+        CTL_KERN,
+        KERN_ARGMAX,
+        0,
+        0
+    };
+
+    int argMax = 0;
+    size_t argMaxSize = sizeof(argMax);
+    // fetch the maximum process arguments size
+    sysctl(mib, 2, &argMax, &argMaxSize, NULL, 0);
+    char *processArguments = (char*) malloc(argMax);
+
+    mib[1] = KERN_PROC;
+    mib[2] = KERN_PROC_ALL;
+    size_t processTableSize = 0;
+    // fetch the kernel process table size
+    sysctl(mib, 4, NULL, &processTableSize, NULL, 0);
+    struct kinfo_proc *processTable = (kinfo_proc*) malloc(processTableSize);
+
+    // fetch the process table
+    sysctl(mib, 4, processTable, &processTableSize, NULL, 0);
+
+    QList<ProcessInfo> processes;
+    for (size_t i = 0; i < (processTableSize / sizeof(struct kinfo_proc)); ++i) {
+        struct kinfo_proc *process = processTable + i;
+
+        ProcessInfo processInfo;
+        processInfo.id = process->kp_proc.p_pid;
+
+        mib[1] = KERN_PROCARGS2;
+        mib[2] = process->kp_proc.p_pid;
+        mib[3] = 0;
+
+        size_t size = argMax;
+        // fetch the process arguments
+        if (sysctl(mib, 3, processArguments, &size, NULL, 0) != -1) {
+            /*
+             * |-----------------| <-- data returned by sysctl()
+             * |      argc       |
+             * |-----------------|
+             * | executable path |
+             * |-----------------|
+             * |    arguments    |
+             * ~~~~~~~~~~~~~~~~~~~
+             * |-----------------|
+            */
+            processInfo.name = QString::fromLocal8Bit(processArguments + sizeof(int));
+        } else {
+            // if we fail, use the name from the process table
+            processInfo.name = QString::fromLocal8Bit(process->kp_proc.p_comm);
+        }
+        processes.append(processInfo);
+    }
+    free(processTable);
+    free(processArguments);
+
+    return processes;
 }
 
 } // namespace KDUpdater
