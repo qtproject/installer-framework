856d3bb01ae91e41770c4e5a1ae74ffcfdfed594
Tim Jenssen <tim.jenssen@nokia.com>
2012-05-07 15:15:49 +0200

    remove unneeded method in the uninstall case
     - an uninstallation is basically running the undo operation on all
       ran/saved operations, for this no scripts are needed and usually
       they are not loaded to run the uninstaller(maybe there is a bug if
       the user used the previous button of the maintenance tool in the
       packagemanager or updater mode)
     - both removed methods using things from script which shouldn't load
       in the uninstall only mode
     - what should do an uninstaller if they need to close an application
       to run correctly, till now this is not needed because if the delete
       method of the installer can't remove a file it moves it to the temp
       directory
    
    
    Change-Id: Ia8d8337b64450473fd400a4c5b2fc2d96daefecc
    Reviewed-by: Karsten Heimrich <karsten.heimrich@nokia.com>

--- src/libs/installer/packagemanagercore_p.cpp
+++ src/libs/installer/packagemanagercore_p.cpp
@@ -1722,10 +1722,6 @@ void PackageManagerCorePrivate::runUninstaller()
         const int uninstallOperationCount = countProgressOperations(undoOperations);
         const double undoOperationProgressSize = double(1) / double(uninstallOperationCount);
 
-        //yes uninstallation is like an update on the component so please inform the user to stop processes
-        callBeginInstallation(m_core->availableComponents());
-        stopProcessesForUpdates(m_core->availableComponents());
-
         runUndoOperations(undoOperations, undoOperationProgressSize, adminRightsGained, false);
         // No operation delete here, as all old undo operations are deleted in the destructor.
 
