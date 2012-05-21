143d6a9f77f0f42485105103b66302aeff9524dd
Niels Weber <niels.2.weber@nokia.com>
2012-05-21 12:33:10 +0200

    line length fixing
    Change-Id: I4e625eac370bfa195fdc4e79e4edf9c4caa54792
    Reviewed-by: Alexander Lenhardt <alexander.lenhardt@nokia.com>

--- src/libs/installer/adminauthorization_x11.cpp
+++ src/libs/installer/adminauthorization_x11.cpp
@@ -71,7 +71,9 @@ static QString getPassword(QWidget *)
 {
     if (QApplication::type() == QApplication::GuiClient) {
         bool ok = false;
-        const QString result = QInputDialog::getText(0, QObject::tr( "Authorization required" ), QObject::tr( "Enter your password to authorize for sudo:" ), QLineEdit::Password, QString(), &ok);
+        const QString result = QInputDialog::getText(0, QObject::tr("Authorization required"),
+                                                     QObject::tr("Enter your password to authorize for sudo:"),
+                                                     QLineEdit::Password, QString(), &ok);
         return ok ? result : QString();
     } else {
         std::cout << QObject::tr("Authorization required").toStdString() << std::endl;
@@ -85,7 +87,8 @@ static QString getPassword(QWidget *)
 static void printError(QWidget *parent, const QString &value)
 {
     if (QApplication::type() == QApplication::GuiClient)
-        QMessageBox::critical(parent, QObject::tr( "Error acquiring admin rights" ), value, QMessageBox::Ok, QMessageBox::Ok);
+        QMessageBox::critical(parent, QObject::tr( "Error acquiring admin rights" ), value,
+                              QMessageBox::Ok, QMessageBox::Ok);
     else
         std::cout << value.toStdString() << std::endl;
 }
