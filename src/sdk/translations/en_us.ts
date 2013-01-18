qtproject/installer-framework

commit fd8e2a3c6b76f164927f55714a2eb22bfda13f61

Translations: Remove translations mentioning Qt 5.0.0 Beta 1.
The translations should be generic, not targeting a specific product.

Change-Id: Id7cdbcf64c0db44458b2ca4606fd4cc4f291e3eb
Reviewed-by: Niels Weber <niels.weber@digia.com>

src/sdk/translations/en_us.ts
@@ -1355,12 +1355,11 @@ Error while loading %2</source>
     <message>
         <location filename="../../libs/installer/packagemanagergui.cpp" line="868"/>
         <source>I accept the licenses.</source>
-        <translation>I h&lt;u&gt;a&lt;/u&gt;ve read and agree to the following terms contained in the license agreements accompanying the Qt SDK and additional items. I agree that my use of the Qt SDK is governed by the terms and conditions contained in these license agreements.</translation>
-    </message>
+        <translation type="unfinished"></translation>
     <message>
         <location filename="../../libs/installer/packagemanagergui.cpp" line="880"/>
         <source>I do not accept the licenses.</source>
-        <translation>I &lt;u&gt;d&lt;/u&gt;o not accept the terms and conditions of the above listed license agreements. Please note by checking the box, you must cancel the installation or downloading the Qt SDK and must destroy all copies, or portions thereof, of the Qt SDK in your possessions.</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../libs/installer/packagemanagergui.cpp" line="873"/>

src/sdk/translations/de_de.ts
@@ -1359,12 +1359,12 @@ Fehler beim Laden von %2</translation>
     <message>
         <location filename="../../libs/installer/packagemanagergui.cpp" line="880"/>
         <source>I do not accept the licenses.</source>
-        <translation>Ich akzeptiere die Bedingungen der obigen Lizenzvereinbarungen nicht. Bitte beachten Sie, dass diese Auswahl bedeutet, dass Sie die Installation und das Herunterladen des Qt SDK abbrechen und dass Sie alle Kopien des Qt SDK und alle Teile davon, die sich in Ihrem Besitz befinden, löschen müssen.</translation>
+        <translation>Ich akzeptiere die Lizenzvereinbarungen nicht.</translation>
     </message>
     <message>
         <location filename="../../libs/installer/packagemanagergui.cpp" line="868"/>
         <source>I accept the licenses.</source>
-        <translation>Ich habe die Bedingungen der folgenden Lizenzvereinbarungen zum Qt SDK und dazugehörigen Bestandteilen gelesen und akzeptiert. Ich bin damit einverstanden, dass meine Benutzung des Qt SDK durch die Bedingungen in diesen Lizenzvereinbarungen bestimmt wird.</translation>
+        <translation>Ich akzeptiere die Lizenzvereinbarungen.</translation>
     </message>
     <message>
         <location filename="../../libs/installer/packagemanagergui.cpp" line="873"/>

commit 642e0d7019e864208def455b887e8cd85771ac12

Fix translation file
Fix translation file format that broke in fd8e2a3c6b76f164927f55 .

Change-Id: Idc225e7261ad09372e936446d82bd6e6ca3871e5
Reviewed-by: Niels Weber <niels.weber@digia.com>

src/sdk/translations/en_us.ts
@@ -1356,6 +1356,7 @@ Error while loading %2</source>
         <location filename="../../libs/installer/packagemanagergui.cpp" line="868"/>
         <source>I accept the licenses.</source>
         <translation type="unfinished"></translation>
+    </message>
     <message>
         <location filename="../../libs/installer/packagemanagergui.cpp" line="880"/>
         <source>I do not accept the licenses.</source>