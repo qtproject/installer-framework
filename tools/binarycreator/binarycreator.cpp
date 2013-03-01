fad480d0318259c0ee45908df7b1245e7c8f3cc6
Niels Weber <niels.weber@digia.com>
2013-03-01 14:45:49 +0100

    binarycreator still needs config option
    Change-Id: Ied61160ee377bf48b2f1a80ca71cdd1e24798a55
    Reviewed-by: Karsten Heimrich <karsten.heimrich@digia.com>

--- tools/binarycreator/binarycreator.cpp
+++ tools/binarycreator/binarycreator.cpp
@@ -440,6 +440,8 @@ static void printUsage()
 
     QInstallerTools::printRepositoryGenOptions();
 
+    std::cout << "  -c|--config file          The file containing the installer configuration" << std::endl;
+
     std::cout << "  -n|--online-only         Don't add any package into the installer" << std::endl;
     std::cout << "                            (for online only installers)" << std::endl;
 
