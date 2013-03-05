0bda027c3431bd8e5c2681cdafeb5cf3d9baf362
Tim Jenssen <tim.jenssen@digia.com>
2013-03-05 13:48:33 +0100

    add a modify extract operation example
    Change-Id: Ia7fea3c3e827c67a03ff4d5bdf5cb464deeeef5c
    Reviewed-by: Niels Weber <niels.weber@digia.com>
    Reviewed-by: Karsten Heimrich <karsten.heimrich@digia.com>
    Reviewed-by: Kai Koehne <kai.koehne@digia.com>

---
+++ examples/modifyextract/packages/org.qtproject.ifw.example.modifyextract/meta/installscript.js
@@ -0,0 +1,12 @@
+function Component()
+{
+}
+
+Component.prototype.createOperationsForArchive = function(archive)
+{
+    // don't use the default operation
+    // component.createOperationsForArchive(archive);
+
+    // add an extract operation with a modified path
+    component.addOperation("Extract", archive, "@TargetDir@/extractToAnotherPath");
+}
