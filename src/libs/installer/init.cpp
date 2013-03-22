qtproject/installer-framework

commit 4cde7b2ed657e5cc2f5f7c071f4ff0a7dd20d56b

Fix missing wizard icon on a windows static build.
Q_IMPORT_PLUGIN() macros are needed in application code
only for Qt4, in Qt5 those are automatically generated by qmake.

Change-Id: Ic75795be402592a0595bb3aa084d0b7ee9a9f8c5
Reviewed-by: Niels Weber <niels.weber@digia.com>
Reviewed-by: Karsten Heimrich <karsten.heimrich@digia.com>
Reviewed-by: Tim Jenssen <tim.jenssen@digia.com>

src/libs/installer/init.cpp
@@ -85,6 +85,8 @@
 
 #include <unix/C/7zCrc.h>
 
+#include <QtPlugin>
+
 #include <iostream>
 
 namespace NArchive {
@@ -151,10 +153,17 @@ static void initArchives()
     CrcGenerateTable();
 }
 
+#if defined(QT_STATIC)
 static void initResources()
 {
     Q_INIT_RESOURCE(patch_file_lists);
+    Q_INIT_RESOURCE(installer);
+# if QT_VERSION < 0x050000
+    Q_IMPORT_PLUGIN(qico)
+    Q_UNUSED(qt_plugin_instance_qico());
+# endif
 }
+#endif
 
 static QByteArray trimAndPrepend(QtMsgType type, const QByteArray &msg)
 {
@@ -220,7 +229,10 @@ void messageHandler(QtMsgType type, const QMessageLogContext &context, const QSt
 
 void QInstaller::init()
 {
+#if defined(QT_STATIC)
+    ::initArchives();
     ::initResources();
+#endif
 
     UpdateOperationFactory &factory = UpdateOperationFactory::instance();
     factory.registerUpdateOperation<CreateShortcutOperation>(QLatin1String("CreateShortcut"));
@@ -261,9 +273,6 @@ void QInstaller::init()
 #ifdef Q_OS_MAC
     factory.registerUpdateOperation<MacReplaceInstallNamesOperation>(QLatin1String("ReplaceInstallNames"));
 #endif // Q_OS_MAC
-
-    // load 7z stuff, if we're a static lib
-    ::initArchives();
 
    // qDebug -> verbose()
 #if QT_VERSION < 0x050000

installerfw.pri
@@ -82,12 +82,14 @@ macx:LIBS += -framework Carbon -framework Security
 isEqual(QT_MAJOR_VERSION, 4) {
     CONFIG += uitools
     CONFIG(static, static|shared) {
+        QTPLUGIN += qico
         DEFINES += QT_STATIC
         QT += script network xml
     }
 } else {
     QT += uitools core-private
     CONFIG(static, static|shared) {
+        QTPLUGIN += qico
         QT += concurrent network script xml
     }
 }

src/libs/installer/fileutils.cpp
@@ -49,6 +49,7 @@
 #include <QtCore/QTemporaryFile>
 #include <QtCore/QThread>
 #include <QtCore/QUrl>
+#include <QImageReader>
 
 #include <errno.h>
 
@@ -557,20 +558,21 @@ typedef struct {
 
 void QInstaller::setApplicationIcon(const QString &application, const QString &icon)
 {
-    wchar_t* const path = new wchar_t[application.length() + 1];
-    QDir::toNativeSeparators(application).toWCharArray(path);
-    path[application.length()] = 0;
-
-    HANDLE updateRes = BeginUpdateResource(path, false);
-    delete[] path;
-
     QFile iconFile(icon);
-    if (!iconFile.open(QIODevice::ReadOnly))
+    if (!iconFile.open(QIODevice::ReadOnly)) {
+        qWarning() << QString::fromLatin1("Could not use '%1' as application icon: %2.")
+            .arg(icon, iconFile.errorString());
         return;
+    }
+
+    if (QImageReader::imageFormat(icon) != "ico") {
+        qWarning() << QString::fromLatin1("Could not use '%1' as application icon, unsupported format %2.")
+            .arg(icon, QLatin1String(QImageReader::imageFormat(icon)));
+        return;
+    }
 
     QByteArray temp = iconFile.readAll();
-
-    ICONDIR* ig = reinterpret_cast< ICONDIR* >(temp.data());
+    ICONDIR* ig = reinterpret_cast<ICONDIR*> (temp.data());
 
     DWORD newSize = sizeof(GRPICONDIR) + sizeof(GRPICONDIRENTRY) * (ig->idCount - 1);
     GRPICONDIR* newDir = reinterpret_cast< GRPICONDIR* >(new char[newSize]);
@@ -578,6 +580,7 @@ void QInstaller::setApplicationIcon(const QString &application, const QString &i
     newDir->idType = ig->idType;
     newDir->idCount = ig->idCount;
 
+    HANDLE updateRes = BeginUpdateResourceW((wchar_t*)QDir::toNativeSeparators(application).utf16(), false);
     for (int i = 0; i < ig->idCount; ++i) {
         char* temp1 = temp.data() + ig->idEntries[i].dwImageOffset;
         DWORD size1 = ig->idEntries[i].dwBytesInRes;
@@ -591,16 +594,16 @@ void QInstaller::setApplicationIcon(const QString &application, const QString &i
         newDir->idEntries[i].dwBytesInRes = ig->idEntries[i].dwBytesInRes;
         newDir->idEntries[i].nID = i + 1;
 
-        UpdateResource(updateRes, RT_ICON, MAKEINTRESOURCE(i + 1),
+        UpdateResourceW(updateRes, RT_ICON, MAKEINTRESOURCE(i + 1),
             MAKELANGID(LANG_NEUTRAL, SUBLANG_NEUTRAL), temp1, size1);
     }
 
-    UpdateResource(updateRes, RT_GROUP_ICON, L"IDI_ICON1", MAKELANGID(LANG_NEUTRAL, SUBLANG_NEUTRAL), newDir
-        , newSize);
+    UpdateResourceW(updateRes, RT_GROUP_ICON, L"IDI_ICON1", MAKELANGID(LANG_NEUTRAL, SUBLANG_NEUTRAL),
+        newDir, newSize);
 
     delete [] newDir;
 
-    EndUpdateResource(updateRes, false);
+    EndUpdateResourceW(updateRes, false);
 }
 
 #endif

commit 478243c584c520d603af70eb797080cea1cf6afe

To be able to use 7z, we need to initialize archives.
Broke that in 4cde7b2ed657e5cc2f5f7c071f4ff0a7dd20d56b

Change-Id: I8a31417ffcf0ae9d8e35906d0bcccd479600d7cf
Reviewed-by: Tim Jenssen <tim.jenssen@digia.com>
Reviewed-by: Niels Weber <niels.weber@digia.com>

src/libs/installer/init.cpp
@@ -229,8 +229,8 @@ void messageHandler(QtMsgType type, const QMessageLogContext &context, const QSt
 
 void QInstaller::init()
 {
-#if defined(QT_STATIC)
     ::initArchives();
+#if defined(QT_STATIC)
     ::initResources();
 #endif
 