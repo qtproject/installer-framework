qtproject/installer-framework

commit 2a73db7e34bd2a9f61455aad73df68294dd1b22c

Fix freeze after rejecting UAC account (check GetLastError()).
Fixes too the case with runing a shared build. Since the
environment is not shared, we would not be able to load
missing libs, thus failing to start the server and endless
loop the wait for the servers connection.

Change-Id: If4c50783eb86488b3c2e0a92357750d9c17e7750
Reviewed-by: Tim Jenssen <tim.jenssen@digia.com>

src/libs/installer/fsengineclient.cpp
@@ -783,7 +783,8 @@ void FSEngineClientHandler::Private::maybeStartServer()
         if (!serverStarted) {
             // something went wrong with authorizing, either user pressed cancel or entered
             // wrong password
-            const QString fallback = serverCommand + QLatin1String(" ") + serverArguments.join(QLatin1String(" "));
+            const QString fallback = serverCommand + QLatin1String(" ") + serverArguments
+                .join(QLatin1String(" "));
 
             const QMessageBox::Button res =
                 QInstaller::MessageBoxHandler::critical(QInstaller::MessageBoxHandler::currentBestSuitParent(),
@@ -800,11 +801,9 @@ void FSEngineClientHandler::Private::maybeStartServer()
         serverStarted = QProcess::startDetached(serverCommand, serverArguments);
     }
 
-    // now wait for the socket to arrive
-    QTcpSocket s;
-    while (serverStarting && serverStarted) {
-        if (FSEngineClientHandler::instance().connect(&s))
-            serverStarting = false;
+    if (serverStarted) {
+        QTcpSocket s;   // now wait for the socket to arrive
+        serverStarted = FSEngineClientHandler::instance().connect(&s);
     }
     serverStarting = false;
 }

src/libs/installer/adminauthorization_win.cpp
@@ -33,12 +33,24 @@
 
 #include "utils.h"
 
-#include <QtCore/QDebug>
-#include <QtCore/QDir>
-#include <QtCore/QStringList>
-#include <QtCore/QVector>
+#include <QDebug>
+#include <QDir>
 
 #include <windows.h>
+
+struct DeCoInitializer
+{
+    DeCoInitializer()
+        : neededCoInit(CoInitialize(NULL) == S_OK)
+    {
+    }
+    ~DeCoInitializer()
+    {
+        if (neededCoInit)
+            CoUninitialize();
+    }
+    bool neededCoInit;
+};
 
 AdminAuthorization::AdminAuthorization()
 {
@@ -74,30 +86,21 @@ bool AdminAuthorization::hasAdminRights()
 
 bool AdminAuthorization::execute(QWidget *, const QString &program, const QStringList &arguments)
 {
+    DeCoInitializer _;
+
     const QString file = QDir::toNativeSeparators(program);
     const QString args = QInstaller::createCommandline(QString(), arguments);
 
-    const int len = GetShortPathName((wchar_t *)file.utf16(), 0, 0);
-    if (len == 0)
-        return false;
-    wchar_t *const buffer = new wchar_t[len];
-    GetShortPathName((wchar_t *)file.utf16(), buffer, len);
+    SHELLEXECUTEINFOW shellExecuteInfo = { 0 };
+    shellExecuteInfo.nShow = SW_HIDE;
+    shellExecuteInfo.lpVerb = L"runas";
+    shellExecuteInfo.lpFile = (wchar_t *)file.utf16();
+    shellExecuteInfo.cbSize = sizeof(SHELLEXECUTEINFOW);
+    shellExecuteInfo.lpParameters = (wchar_t *)args.utf16();
 
-    SHELLEXECUTEINFOW TempInfo = { 0 };
-    TempInfo.cbSize = sizeof(SHELLEXECUTEINFOW);
-    TempInfo.fMask = 0;
-    TempInfo.hwnd = 0;
-    TempInfo.lpVerb = L"runas";
-    TempInfo.lpFile = buffer;
-    TempInfo.lpParameters = (wchar_t *)args.utf16();
-    TempInfo.lpDirectory = 0;
-    TempInfo.nShow = SW_NORMAL;
+    qDebug() << QString::fromLatin1("Starting elevated process %1 with arguments: %2.").arg(file, args);
+    ShellExecuteExW(&shellExecuteInfo);
+    qDebug() << "Finished starting elevated process.";
 
-    
-    qDebug() << QString::fromLatin1(" starting elevated process %1 %2 with ::ShellExecuteExW( &TempInfo );"
-        ).arg(program, arguments.join(QLatin1String(" ")));
-    const bool result = ::ShellExecuteEx(&TempInfo);
-    qDebug() << QLatin1String("after starting elevated process");
-    delete[] buffer;
-    return result;
+    return GetLastError() == ERROR_SUCCESS;
 }

commit 5fb7579d640d5007b641309cb1734b6e0bf2b1cc

Fix race condition when connecting to fs process
Fixes a regression introduced by 2a73db7e34b: We only tried to
connect to the port 3 times immediately after the fs process was
started, which might be too short.

Instead, expicitly try to connect for 30 seconds, and only give up
then.

Task-number: QTIFW-316
Change-Id: I8a1ba59ff680f4ef536f13230215f2289ab3b0ed
Reviewed-by: Niels Weber <niels.weber@digia.com>

src/libs/installer/fsengineclient.cpp
@@ -44,6 +44,8 @@
 #include "adminauthorization.h"
 #include "messageboxhandler.h"
 
+#include <QElapsedTimer>
+
 #include <QtCore/QCoreApplication>
 #include <QtCore/QMutex>
 #include <QtCore/QProcess>
@@ -809,8 +811,14 @@ void FSEngineClientHandler::Private::maybeStartServer()
     }
 
     if (serverStarted) {
-        QTcpSocket s;   // now wait for the socket to arrive
-        serverStarted = FSEngineClientHandler::instance().connect(&s);
+        QElapsedTimer t;
+        t.start();
+        while (serverStarting && serverStarted
+               && t.elapsed() < 30000) { // 30 seconds ought to be enough for the app to start
+            QTcpSocket s;
+            if (FSEngineClientHandler::instance().connect(&s))
+                serverStarting = false;
+        }
     }
     serverStarting = false;
 }