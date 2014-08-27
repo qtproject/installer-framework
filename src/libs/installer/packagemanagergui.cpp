qtproject/installer-framework

commit 2f5c612538984122018e118b506119dd0e2a2e6e

Toggle details on ReadyForInstallation page with verbose mode
Removed the Details button, as it only confused people,
especially with the internal component names being shown
there. As it is essentially debugging output, it should
be toggled with the verbose switch, as is the rest of the
debugging output.

Task-number: QTIFW-140
Change-Id: I23dc5bd033a52cffc7cf811b6ec886a9db3a6a63
Reviewed-by: Karsten Heimrich <karsten.heimrich@digia.com>

src/libs/installer/packagemanagergui.cpp
@@ -1939,13 +1939,6 @@ ReadyForInstallationPage::ReadyForInstallationPage(PackageManagerCore *core)
     topLayout->addWidget(m_msgLabel);
     baseLayout->addLayout(topLayout);
 
-    m_taskDetailsButton = new QPushButton(tr("&Show Details"), this);
-    m_taskDetailsButton->setFocusPolicy(Qt::NoFocus);
-    m_taskDetailsButton->setObjectName(QLatin1String("TaskDetailsButton"));
-    m_taskDetailsButton->setSizePolicy(QSizePolicy::Maximum, QSizePolicy::Fixed);
-    connect(m_taskDetailsButton, SIGNAL(clicked()), this, SLOT(toggleDetails()));
-    topLayout->addWidget(m_taskDetailsButton);
-
     QVBoxLayout *bottomLayout = new QVBoxLayout();
     bottomLayout->setObjectName(QLatin1String("BottomLayout"));
     bottomLayout->addStretch();
@@ -1970,8 +1963,12 @@ void ReadyForInstallationPage::entering()
 {
     setCommitPage(false);
 
+    if (isVerbose())
+        m_taskDetailsBrowser->setVisible(true);
+    else
+        m_taskDetailsBrowser->setVisible(false);
+
     if (packageManagerCore()->isUninstaller()) {
-        m_taskDetailsButton->setVisible(false);
         m_taskDetailsBrowser->setVisible(false);
         setButtonText(QWizard::CommitButton, tr("U&ninstall"));
         setColoredTitle(tr("Ready to Uninstall"));
@@ -2103,8 +2100,6 @@ void ReadyForInstallationPage::refreshTaskDetailsBrowser()
             }
             htmlOutput.append(QLatin1String("</ul>"));
             m_taskDetailsBrowser->setHtml(htmlOutput);
-            if (!m_taskDetailsBrowser->isVisible())
-                toggleDetails();
             setCommitPage(false);
             return;
     }
@@ -2132,13 +2127,6 @@ void ReadyForInstallationPage::refreshTaskDetailsBrowser()
         htmlOutput.append(QString::fromLatin1("<li> %1 </li>").arg(component->name()));
     }
     m_taskDetailsBrowser->setHtml(htmlOutput);
-}
-
-void ReadyForInstallationPage::toggleDetails()
-{
-    const bool visible = !m_taskDetailsBrowser->isVisible();
-    m_taskDetailsBrowser->setVisible(visible);
-    m_taskDetailsButton->setText(visible ? tr("&Hide Details") : tr("&Show Details"));
 }
 
 void ReadyForInstallationPage::leaving()

src/libs/installer/packagemanagergui.h
@@ -395,9 +395,6 @@ public:
 
     bool isComplete() const;
 
-private slots:
-    void toggleDetails();
-
 protected:
     void entering();
     void leaving();
@@ -407,7 +404,6 @@ private:
 
 private:
     QLabel *m_msgLabel;
-    QPushButton *m_taskDetailsButton;
     QTextBrowser* m_taskDetailsBrowser;
 };
 

commit 0931738b103962b01b1c429c17e3e0b7fe09a4aa

Show the details text box when dependencies are not met.
This was broken in change 2f5c612538984122018e118b506119dd0e2a2e6e
It stopped working when verbose (-v) option was not provided.

Change-Id: I1f269faf9b8ce14cd40508c5f7622504fb3f373d
Reviewed-by: Kai Koehne <kai.koehne@digia.com>

src/libs/installer/packagemanagergui.cpp
@@ -1963,11 +1963,6 @@ void ReadyForInstallationPage::entering()
 {
     setCommitPage(false);
 
-    if (isVerbose())
-        m_taskDetailsBrowser->setVisible(true);
-    else
-        m_taskDetailsBrowser->setVisible(false);
-
     if (packageManagerCore()->isUninstaller()) {
         m_taskDetailsBrowser->setVisible(false);
         setButtonText(QWizard::CommitButton, tr("U&ninstall"));
@@ -1992,7 +1987,11 @@ void ReadyForInstallationPage::entering()
             .arg(productName()));
     }
 
-    refreshTaskDetailsBrowser();
+    QString htmlOutput;
+    bool componentsOk = calculateComponents(&htmlOutput);
+    m_taskDetailsBrowser->setHtml(htmlOutput);
+    m_taskDetailsBrowser->setVisible(!componentsOk || isVerbose());
+    setCommitPage(componentsOk);
 
     const VolumeInfo tempVolume = VolumeInfo::fromPath(QDir::tempPath());
     const VolumeInfo targetVolume = VolumeInfo::fromPath(packageManagerCore()->value(scTargetDir));
@@ -2005,7 +2004,6 @@ void ReadyForInstallationPage::entering()
         qDebug() << QString::fromLatin1("Could not determine available space on device. Volume "
             "descriptor: %1, Mount path: %2. Continue silently.").arg(targetVolume
             .volumeDescriptor(), targetVolume.mountPath());
-        setCommitPage(true);
         return;     // TODO: Shouldn't this also disable the "Next" button?
     }
 
@@ -2050,6 +2048,7 @@ void ReadyForInstallationPage::entering()
             "installation! Available space: %1, at least required %2.")
             .arg(humanReadableSize(installVolumeAvailableSize),
             humanReadableSize(required + tempRequired)));
+        setCommitPage(false);
         return;
     }
 
@@ -2057,6 +2056,7 @@ void ReadyForInstallationPage::entering()
         m_msgLabel->setText(tr("Not enough disk space to store all selected components! Available "
             "space: %1, at least required: %2.").arg(humanReadableSize(installVolumeAvailableSize),
             humanReadableSize(required)));
+        setCommitPage(false);
         return;
     }
 
@@ -2064,6 +2064,7 @@ void ReadyForInstallationPage::entering()
         m_msgLabel->setText(tr("Not enough disk space to store temporary files! Available space: "
             "%1, at least required: %2.").arg(humanReadableSize(tempVolumeAvailableSize),
             humanReadableSize(tempRequired)));
+        setCommitPage(false);
         return;
     }
 
@@ -2081,27 +2082,25 @@ void ReadyForInstallationPage::entering()
 
     m_msgLabel->setText(QString::fromLatin1("%1 %2").arg(m_msgLabel->text(),
             tr("Installation will use %1 of disk space.").arg(humanReadableSize(required))));
-
-    setCommitPage(true);
 }
 
-void ReadyForInstallationPage::refreshTaskDetailsBrowser()
+bool ReadyForInstallationPage::calculateComponents(QString *displayString)
 {
     QString htmlOutput;
     QString lastInstallReason;
     if (!packageManagerCore()->calculateComponentsToUninstall() ||
-        !packageManagerCore()->calculateComponentsToInstall()) {
-            htmlOutput.append(QString::fromLatin1("<h2><font color=\"red\">%1</font></h2><ul>")
-                .arg(tr("Can not resolve all dependencies!")));
-            //if we have a missing dependency or a recursion we can display it
-            if (!packageManagerCore()->componentsToInstallError().isEmpty()) {
-                htmlOutput.append(QString::fromLatin1("<li> %1 </li>").arg(
-                    packageManagerCore()->componentsToInstallError()));
-            }
-            htmlOutput.append(QLatin1String("</ul>"));
-            m_taskDetailsBrowser->setHtml(htmlOutput);
-            setCommitPage(false);
-            return;
+            !packageManagerCore()->calculateComponentsToInstall()) {
+        htmlOutput.append(QString::fromLatin1("<h2><font color=\"red\">%1</font></h2><ul>")
+                          .arg(tr("Can not resolve all dependencies!")));
+        //if we have a missing dependency or a recursion we can display it
+        if (!packageManagerCore()->componentsToInstallError().isEmpty()) {
+            htmlOutput.append(QString::fromLatin1("<li> %1 </li>").arg(
+                                  packageManagerCore()->componentsToInstallError()));
+        }
+        htmlOutput.append(QLatin1String("</ul>"));
+        if (displayString)
+            *displayString = htmlOutput;
+        return false;
     }
 
     // In case of updater mode we don't uninstall components.
@@ -2126,7 +2125,9 @@ void ReadyForInstallationPage::refreshTaskDetailsBrowser()
         }
         htmlOutput.append(QString::fromLatin1("<li> %1 </li>").arg(component->name()));
     }
-    m_taskDetailsBrowser->setHtml(htmlOutput);
+    if (displayString)
+        *displayString = htmlOutput;
+    return true;
 }
 
 void ReadyForInstallationPage::leaving()

src/libs/installer/packagemanagergui.h
@@ -400,7 +400,7 @@ protected:
     void leaving();
 
 private:
-    void refreshTaskDetailsBrowser();
+    bool calculateComponents(QString *displayString);
 
 private:
     QLabel *m_msgLabel;