9a5127a9d99ba2650e8a1f4c16c82876b7b915f9
Leena Miettinen <riitta-leena.miettinen@theqtcompany.com>
2014-12-22 16:08:16 +0100

    Doc: update docs for the Quit Installer example
    Change-Id: I0b456962e9a07e16a3e44882d936f604fcd4f71f
    Reviewed-by: Kai Koehne <kai.koehne@theqtcompany.com>

--- examples/quit_installer/packages/org.qtproject.ifw.example.quitinstaller/meta/installscript.js
+++ examples/quit_installer/packages/org.qtproject.ifw.example.quitinstaller/meta/installscript.js
@@ -35,9 +35,9 @@
 function Component()
 {
     var result = QMessageBox["question"]("test.quit", "Installer", "Do you want to quit the installer?<br>" +
-        "This message box was created through javascript.", QMessageBox.Ok | QMessageBox.Cancel);
+        "This message box was created using JavaScript.", QMessageBox.Ok | QMessageBox.Cancel);
     if (result == QMessageBox.Ok) {
-        installer.setValue("FinishedText", "<font color='red' size=10>This installer was quit.</font>");
+        installer.setValue("FinishedText", "<font color='red' size=3>The installer was quit.</font>");
         installer.setDefaultPageVisible(QInstaller.TargetDirectory, false);
         installer.setDefaultPageVisible(QInstaller.ReadyForInstallation, false);
         installer.setDefaultPageVisible(QInstaller.ComponentSelection, false);
@@ -47,6 +47,6 @@ function Component()
         gui.clickButton(buttons.NextButton);
     } else {
         installer.setValue("FinishedText",
-            "<font color='green' size=10>The installer was not quit from javascript.</font>");
+            "<font color='green' size=3>The installer was not quit by JavaScript.</font>");
     }
 }
