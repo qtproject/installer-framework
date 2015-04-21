55eb4ba17e8729cf47449ff1f4f06ca93b837f9e
Jarek Kobus <jaroslaw.kobus@theqtcompany.com>
2015-04-21 15:15:48 +0200

    Don't modify check state of autodependent component when Select All
    Change-Id: Id50fc5fa3460ffda62102019b46923b01b8e8745
    Reviewed-by: Kai Koehne <kai.koehne@theqtcompany.com>

--- src/libs/installer/componentmodel.cpp
+++ src/libs/installer/componentmodel.cpp
@@ -577,7 +577,7 @@ QSet<QModelIndex> ComponentModel::updateCheckedState(const ComponentSet &compone
     // we can start in descending order to check node and tri-state nodes properly
     for (int i = sortedNodes.count(); i > 0; i--) {
         Component * const node = sortedNodes.at(i - 1);
-        if (!node->isCheckable() || !node->isEnabled())
+        if (!node->isCheckable() || !node->isEnabled() || !node->autoDependencies().isEmpty())
             continue;
 
         Qt::CheckState newState = state;
