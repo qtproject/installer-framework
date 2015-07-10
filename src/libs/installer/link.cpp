qtproject/installer-framework

commit b2a65ce68bce4fcd4b517d1aadedb40792bfc818

Sanitize QDebug output
Prefer using the stream operator logic instead of using
QString::fromLatin1. Add a noquote() where a QString should be printed
without any quotes/escapes.

This also fixes the tests with Qt 5.5, where QString's are further
escaped.

Change-Id: Ie3afd14b9355101d3c0b52f30f66ec759c76526c
Reviewed-by: Karsten Heimrich <karsten.heimrich@theqtcompany.com>

src/libs/installer/link.cpp
@@ -94,8 +94,7 @@ public:
             OPEN_EXISTING, FILE_FLAG_OPEN_REPARSE_POINT | FILE_FLAG_BACKUP_SEMANTICS, 0);
 
         if (m_dirHandle == INVALID_HANDLE_VALUE) {
-            qWarning() << QString::fromLatin1("Cannot open: \"%1\": %2\n").arg(path)
-                .arg(QInstaller::windowsErrorString(GetLastError()));
+            qWarning() << "Cannot open" << path << ":" << QInstaller::windowsErrorString(GetLastError());
         }
     }
 
@@ -144,14 +143,12 @@ QString readWindowsSymLink(const QString &path)
 Link createJunction(const QString &linkPath, const QString &targetPath)
 {
     if (!QDir().mkpath(linkPath)) {
-        qWarning() << QString::fromLatin1("Cannot create the mount directory: %1").arg(
-            linkPath);
+        qWarning() << "Cannot create the mount directory" << linkPath;
         return Link(linkPath);
     }
     FileHandleWrapper dirHandle(linkPath);
     if (dirHandle.handle() == INVALID_HANDLE_VALUE) {
-        qWarning() << QString::fromLatin1("Cannot open: \"%1\": %2\n").arg(linkPath)
-            .arg(QInstaller::windowsErrorString(GetLastError()));
+        qWarning() << "Cannot open" << linkPath << ":" << QInstaller::windowsErrorString(GetLastError());
         return Link(linkPath);
     }
 
@@ -181,8 +178,8 @@ Link createJunction(const QString &linkPath, const QString &targetPath)
     if (!::DeviceIoControl(dirHandle.handle(), FSCTL_SET_REPARSE_POINT, reparseStructData,
         reparseStructData->ReparseDataLength + REPARSE_DATA_BUFFER_HEADER_SIZE, 0, 0,
         &bytesReturned, 0)) {
-            qWarning() << QString::fromLatin1("Cannot set the reparse point for \"%1\" to %2: %3"
-                ).arg(linkPath, targetPath).arg(QInstaller::windowsErrorString(GetLastError()));
+            qWarning() << "Cannot set the reparse point for" << linkPath "to" << targetPath
+                       << ":" << QInstaller::windowsErrorString(GetLastError());
     }
     return Link(linkPath);
 }
@@ -203,8 +200,7 @@ bool removeJunction(const QString &path)
             REPARSE_GUID_DATA_BUFFER_HEADER_SIZE, 0, 0,
             &bytesReturned, 0)) {
 
-            qWarning() << QString::fromLatin1("Cannot remove the reparse point \"'%1\": %3"
-                ).arg(path).arg(QInstaller::windowsErrorString(GetLastError()));
+            qWarning() << "Cannot remove the reparse point" << path << ":" << QInstaller::windowsErrorString(GetLastError());
             return false;
         }
     }
@@ -217,8 +213,7 @@ Link createLnSymlink(const QString &linkPath, const QString &targetPath)
     int linkedError = symlink(QFileInfo(targetPath).absoluteFilePath().toUtf8(),
         QFileInfo(linkPath).absoluteFilePath().toUtf8());
     if (linkedError != 0) {
-        qWarning() << QString::fromLatin1("Cannot create a symlink from \"%1\" to \"%2\": %3"
-                    ).arg(linkPath, targetPath).arg(linkedError);
+        qWarning() << "Cannot create a symlink from" << linkPath << "to" << targetPath << ":" << linkedError;
     }
 
 
@@ -245,8 +240,7 @@ Link Link::create(const QString &link, const QString &targetPath)
     if (!linkPathExists)
         linkPathExists = QDir().mkpath(linkPath);
     if (!linkPathExists) {
-        qWarning() << QString::fromLatin1("Cannot create the needed directories: %1").arg(
-            link);
+        qWarning() << "Cannot create the needed directories" << link;
         return Link(link);
     }
 
@@ -254,8 +248,8 @@ Link Link::create(const QString &link, const QString &targetPath)
     if (QFileInfo(targetPath).isDir())
         return createJunction(link, targetPath);
 
-    qWarning() << QString::fromLatin1("At the moment the %1 can not create anything else as "\
-        "junctions for directories under windows").arg(QLatin1String(Q_FUNC_INFO));
+    qWarning() << "At the moment the" << Q_FUNC_INFO << "can not create anything else as "
+               << "junctions for directories under windows";
     return Link(link);
 #else
     return createLnSymlink(link, targetPath);

src/libs/installer/adminauthorization_win.cpp
@@ -112,14 +112,14 @@ bool AdminAuthorization::execute(QWidget *, const QString &program, const QStrin
     shellExecuteInfo.lpParameters = (wchar_t *)args.utf16();
     shellExecuteInfo.fMask = SEE_MASK_NOASYNC;
 
-    qDebug() << QString::fromLatin1("Starting elevated process \"%1\" with arguments \"%2\".").arg(file, args);
+    qDebug() << "Starting elevated process" << file << "with arguments" << args;
 
     if (ShellExecuteExW(&shellExecuteInfo)) {
         qDebug() << "Finished starting elevated process.";
         return true;
     } else {
-        qWarning() << QString::fromLatin1("Error while starting elevated process %1: %2").arg(
-                          program, QInstaller::windowsErrorString(GetLastError()));
+        qWarning() << "Error while starting elevated process" << program
+                   << ":" << QInstaller::windowsErrorString(GetLastError());
     }
     return false;
 }

src/libs/installer/consumeoutputoperation.cpp
@@ -127,8 +127,7 @@ bool ConsumeOutputOperation::performOperation()
 
     }
     if (executableOutput.isEmpty()) {
-        qWarning() << QString::fromLatin1("Cannot get any query output from executable \"%1\".").arg(
-            executable.absoluteFilePath());
+        qWarning() << "Cannot get any query output from executable" << executable.absoluteFilePath();
     }
     core->setValue(installerKeyName, QString::fromLocal8Bit(executableOutput));
     return true;

src/libs/installer/downloadfiletask.cpp
@@ -278,7 +278,7 @@ void Downloader::onSslErrors(const QList<QSslError> &sslErrors)
     Q_UNUSED(sslErrors);
 #else
     foreach (const QSslError &error, sslErrors)
-        qDebug() << QString::fromLatin1("SSL error: %1").arg(error.errorString());
+        qDebug() << "SSL error:" << error.errorString();
 #endif
 }
 

src/libs/installer/elevatedexecuteoperation.cpp
@@ -226,7 +226,7 @@ bool ElevatedExecuteOperation::Private::run(const QStringList &arguments)
         QByteArray standardErrorOutput = process->readAllStandardError();
         // in error case it would be useful to see something in verbose output
         if (!standardErrorOutput.isEmpty())
-            qWarning() << standardErrorOutput;
+            qWarning().noquote() << standardErrorOutput;
 
         returnValue = false;
     }

src/libs/installer/fileutils.cpp
@@ -186,7 +186,7 @@ void QInstaller::removeFiles(const QString &path, bool ignoreErrors)
                             QDir::toNativeSeparators(f.fileName()), f.errorString());
                 if (!ignoreErrors)
                     throw Error(errorMessage);
-                qWarning() << errorMessage;
+                qWarning().noquote() << errorMessage;
             }
         }
     }
@@ -228,7 +228,7 @@ void QInstaller::removeDirectory(const QString &path, bool ignoreErrors)
                                                           errnoToQString(errno));
             if (!ignoreErrors)
                 throw Error(errorMessage);
-            qWarning() << errorMessage;
+            qWarning().noquote() << errorMessage;
         }
     }
 }
@@ -493,14 +493,13 @@ void QInstaller::setApplicationIcon(const QString &application, const QString &i
 {
     QFile iconFile(icon);
     if (!iconFile.open(QIODevice::ReadOnly)) {
-        qWarning() << QString::fromLatin1("Cannot use \"%1\" as application icon: %2")
-            .arg(icon, iconFile.errorString());
+        qWarning() << "Cannot use" << icon << "as an application icon:" << iconFile.errorString();
         return;
     }
 
     if (QImageReader::imageFormat(icon) != "ico") {
-        qWarning() << QString::fromLatin1("Cannot use \"%1\" as application icon, unsupported format %2.")
-            .arg(icon, QLatin1String(QImageReader::imageFormat(icon)));
+        qWarning() << "Cannot use" << icon << "as an application icon, unsupported format"
+                   << QImageReader::imageFormat(icon).constData();
         return;
     }
 

src/libs/installer/installercalculator.cpp
@@ -121,7 +121,7 @@ bool InstallerCalculator::appendComponentsToInstall(const QList<Component *> &co
     foreach (Component *component, components){
         if (m_toInstallComponentIds.contains(component->name())) {
             const QString errorMessage = recursionError(component);
-            qWarning() << errorMessage;
+            qWarning().noquote() << errorMessage;
             m_componentsToInstallError.append(errorMessage);
             Q_ASSERT_X(!m_toInstallComponentIds.contains(component->name()), Q_FUNC_INFO,
                 qPrintable(errorMessage));
@@ -173,7 +173,7 @@ bool InstallerCalculator::appendComponentToInstall(Component *component)
             const QString errorMessage = QCoreApplication::translate("InstallerCalculator",
                 "Cannot find missing dependency \"%1\" for \"%2\".").arg(dependencyComponentName,
                 component->name());
-            qWarning() << errorMessage;
+            qWarning().noquote() << errorMessage;
             m_componentsToInstallError.append(errorMessage);
             return false;
         }
@@ -182,7 +182,7 @@ bool InstallerCalculator::appendComponentToInstall(Component *component)
             && !m_toInstallComponentIds.contains(dependencyComponent->name())) {
                 if (m_visitedComponents.value(component).contains(dependencyComponent)) {
                     const QString errorMessage = recursionError(component);
-                    qWarning() << errorMessage;
+                    qWarning().noquote() << errorMessage;
                     m_componentsToInstallError = errorMessage;
                     Q_ASSERT_X(!m_visitedComponents.value(component).contains(dependencyComponent),
                         Q_FUNC_INFO, qPrintable(errorMessage));

src/libs/installer/installiconsoperation.cpp
@@ -273,10 +273,10 @@ bool InstallIconsOperation::undoOperation()
     }
 
     if (!warningMessages.isEmpty()) {
-        qWarning() << QString::fromLatin1("Undo of operation %1 with arguments \"%2\" had some problems.").arg(
-            name(), arguments().join(QLatin1String(", ")));
+        qWarning() << "Undo of operation" << name() << "with arguments"
+                   << arguments().join(QLatin1String(", ")) << "had some problems.";
         foreach (const QString &message, warningMessages) {
-            qWarning() << message;
+            qWarning().noquote() << message;
         }
     }
 

commit d01de18c1edfc7613f5c521452b595e71cd6e94d

Fix build on Windows
Fix breakage introduced with b2a65ce68

Change-Id: I587e9c7d681e8197fa0c967441b52ef58cfe23e6
Reviewed-by: Christian Stenger <christian.stenger@theqtcompany.com>

src/libs/installer/link.cpp
@@ -178,7 +178,7 @@ Link createJunction(const QString &linkPath, const QString &targetPath)
     if (!::DeviceIoControl(dirHandle.handle(), FSCTL_SET_REPARSE_POINT, reparseStructData,
         reparseStructData->ReparseDataLength + REPARSE_DATA_BUFFER_HEADER_SIZE, 0, 0,
         &bytesReturned, 0)) {
-            qWarning() << "Cannot set the reparse point for" << linkPath "to" << targetPath
+            qWarning() << "Cannot set the reparse point for" << linkPath << "to" << targetPath
                        << ":" << QInstaller::windowsErrorString(GetLastError());
     }
     return Link(linkPath);