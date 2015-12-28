6ba58b42ab30f85928c141574defa35dfe255ef0
Iikka Eklund <iikka.eklund@theqtcompany.com>
2015-12-28 14:40:15 +0200

    Update license headers
    Update existing license headers. LGPL -> LGPLv21.
    Update copyright year as well.
    
    Change-Id: Ie1d71f8c68186b8f625f409ddf94691f178093c9
    Reviewed-by: Katja Marttila <katja.marttila@theqtcompany.com>
    Reviewed-by: Jani Heikkinen <jani.heikkinen@theqtcompany.com>

--- src/libs/installer/downloadfiletask.cpp
+++ src/libs/installer/downloadfiletask.cpp
@@ -1,18 +1,18 @@
 
 /**************************************************************************
 **
-** Copyright (C) 2015 The Qt Company Ltd.
+** Copyright (C) 2016 The Qt Company Ltd.
 ** Contact: http://www.qt.io/licensing/
 **
 ** This file is part of the Qt Installer Framework.
 **
-** $QT_BEGIN_LICENSE:LGPL$
+** $QT_BEGIN_LICENSE:LGPL21$
 ** Commercial License Usage
 ** Licensees holding valid commercial Qt licenses may use this file in
 ** accordance with the commercial license agreement provided with the
 ** Software or, alternatively, in accordance with the terms contained in
 ** a written agreement between you and The Qt Company. For licensing terms
-** and conditions see http://qt.io/terms-conditions. For further
+** and conditions see http://www.qt.io/terms-conditions. For further
 ** information use the contact form at http://www.qt.io/contact-us.
 **
 ** GNU Lesser General Public License Usage
@@ -27,7 +27,6 @@
 ** As a special exception, The Qt Company gives you certain additional
 ** rights. These rights are described in The Qt Company LGPL Exception
 ** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
-**
 **
 ** $QT_END_LICENSE$
 **
