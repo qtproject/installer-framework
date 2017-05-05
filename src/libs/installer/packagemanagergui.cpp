a1694a8ea3e34df7d06cb36275200f08ec54ce59
Andy Shaw <andy.shaw@qt.io>
2017-05-05 13:08:04 +0200

    Clarify the add/remove components string to make it clearer
    Since there is generally confusion as to what will be installed,
    removed or updated the string is clarified further to hopefully
    make it much more clearer what will happen depending on whether
    it is checked or not.
    
    Change-Id: I1077b3dd3e4511e01ca64d0598942d5254e33a54
    Reviewed-by: Leena Miettinen <riitta-leena.miettinen@qt.io>
    Reviewed-by: Katja Marttila <katja.marttila@qt.io>

--- src/libs/installer/packagemanagergui.cpp
+++ src/libs/installer/packagemanagergui.cpp
@@ -2182,7 +2182,7 @@ void ComponentSelectionPage::entering()
         QT_TR_NOOP("Please select the components you want to update."),
         QT_TR_NOOP("Please select the components you want to install."),
         QT_TR_NOOP("Please select the components you want to uninstall."),
-        QT_TR_NOOP("Select the components to install. Deselect installed components to uninstall them.")
+        QT_TR_NOOP("Select the components to install. Deselect installed components to uninstall them. Any components already installed will not be updated.")
      };
 
     int index = 0;
