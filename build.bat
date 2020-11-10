a0a125f9e4fe18e9e5c05223cab7a95f5d85d8a3
Hjalti Kolbeinsson <hjaltik@ccpgames.com>
2020-11-10 14:36:50 +0000

    EO-10244  - Simplified the build script a bit

--- build.bat
+++ build.bat
@@ -2,15 +2,12 @@
 
 SET CLEAN=%1
 
-if "%JOM%"=="" (
-  SET JOM=C:\Qt\Tools\QtCreator\bin\jom.exe
+if "%TOOLS_PREFIX%"=="" (
+  SET TOOLS_PREFIX=C:
 )
 
-if "%QMAKE_PREFIX%"=="" (
-    SET QMAKE_PREFIX=C:
-)
-
-SET QMAKE=%QMAKE_PREFIX%\Qt_static\v5.12.7\bin\qmake.exe
+SET JOM=%TOOLS_PREFIX%\jom\jom.exe
+SET QMAKE=%TOOLS_PREFIX%\Qt_static\v5.12.7\bin\qmake.exe
 SET WIN_SDK=10.0.17763.0
 
 if "%CLEAN%"=="" call clean.bat 1
