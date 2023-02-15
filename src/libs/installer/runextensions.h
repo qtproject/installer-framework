bc4fedad19166a803187b73327d11f05dbbc2cae
Arttu Tarkiainen <arttu.tarkiainen@qt.io>
2023-02-15 14:23:15 +0200

    Simplify QtConcurrent run extensions
    We can user parameter packs (since C++11) to concatenate the
    different overloads of the QtConcurrent::run() -function and
    variants of the classes derived from QRunnable taking a different
    count of parameters.
    
    Change-Id: I501f058dec29ebefbd1cff24da5e4e13c5235e2e
    Reviewed-by: Katja Marttila <katja.marttila@qt.io>

--- src/libs/installer/runextensions.h
+++ src/libs/installer/runextensions.h
@@ -1,6 +1,6 @@
 /**************************************************************************
 **
-** Copyright (C) 2022 The Qt Company Ltd.
+** Copyright (C) 2023 The Qt Company Ltd.
 ** Contact: https://www.qt.io/licensing/
 **
 ** This file is part of the Qt Installer Framework.
@@ -37,12 +37,12 @@ QT_BEGIN_NAMESPACE
 
 namespace QtConcurrent {
 
-template <typename T,  typename FunctionPointer>
-class StoredInterfaceFunctionCall0 : public QRunnable
+template <typename T,  typename FunctionPointer, typename... Args>
+class StoredInterfaceFunctionCall : public QRunnable
 {
 public:
-    StoredInterfaceFunctionCall0(void (fn)(QFutureInterface<T> &))
-    : fn(fn) { }
+    StoredInterfaceFunctionCall(void (fn)(QFutureInterface<T> &, Args...), const Args...)
+    : fn(fn), args(args) { }
 
     QFuture<T> start()
     {
@@ -54,20 +54,20 @@ public:
 
     void run() override
     {
-        fn(futureInterface);
+        fn(futureInterface, std::forward<Args>(args)...);
         futureInterface.reportFinished();
     }
 private:
     QFutureInterface<T> futureInterface;
     FunctionPointer fn;
-
+    std::tuple<Args...> args;
 };
-template <typename T,  typename FunctionPointer, typename Class>
-class StoredInterfaceMemberFunctionCall0 : public QRunnable
+template <typename T,  typename FunctionPointer, typename Class, typename... Args>
+class StoredInterfaceMemberFunctionCall : public QRunnable
 {
 public:
-    StoredInterfaceMemberFunctionCall0(void (Class::*fn)(QFutureInterface<T> &), Class *object)
-    : fn(fn), object(object) { }
+    StoredInterfaceMemberFunctionCall(void (Class::*fn)(QFutureInterface<T> &, Args...), Class *object, const Args...)
+    : fn(fn), object(object), args(args) { }
 
     QFuture<T> start()
     {
@@ -79,342 +79,28 @@ public:
 
     void run() override
     {
-        (object->*fn)(futureInterface);
-        futureInterface.reportFinished();
-    }
-private:
-    QFutureInterface<T> futureInterface;
-    FunctionPointer fn;
-    Class *object;
-
-};
-
-template <typename T,  typename FunctionPointer, typename Arg1>
-class StoredInterfaceFunctionCall1 : public QRunnable
-{
-public:
-    StoredInterfaceFunctionCall1(void (fn)(QFutureInterface<T> &, Arg1), const Arg1 &arg1)
-    : fn(fn), arg1(arg1) { }
-
-    QFuture<T> start()
-    {
-        futureInterface.reportStarted();
-        QFuture<T> future = futureInterface.future();
-        QThreadPool::globalInstance()->start(this);
-        return future;
-    }
-
-    void run() override
-    {
-        fn(futureInterface, arg1);
-        futureInterface.reportFinished();
-    }
-private:
-    QFutureInterface<T> futureInterface;
-    FunctionPointer fn;
-    Arg1 arg1;
-};
-template <typename T,  typename FunctionPointer, typename Class, typename Arg1>
-class StoredInterfaceMemberFunctionCall1 : public QRunnable
-{
-public:
-    StoredInterfaceMemberFunctionCall1(void (Class::*fn)(QFutureInterface<T> &, Arg1), Class *object, const Arg1 &arg1)
-    : fn(fn), object(object), arg1(arg1) { }
-
-    QFuture<T> start()
-    {
-        futureInterface.reportStarted();
-        QFuture<T> future = futureInterface.future();
-        QThreadPool::globalInstance()->start(this);
-        return future;
-    }
-
-    void run() override
-    {
-        (object->*fn)(futureInterface, arg1);
+        (object->*fn)(futureInterface, std::forward<Args>(args)...);
         futureInterface.reportFinished();
     }
 private:
     QFutureInterface<T> futureInterface;
     FunctionPointer fn;
     Class *object;
-    Arg1 arg1;
+    std::tuple<Args...> args;
 };
 
-template <typename T,  typename FunctionPointer, typename Arg1, typename Arg2>
-class StoredInterfaceFunctionCall2 : public QRunnable
+template <typename T, typename... Args>
+QFuture<T> run(void (*functionPointer)(QFutureInterface<T> &, Args...), Args... args)
 {
-public:
-    StoredInterfaceFunctionCall2(void (fn)(QFutureInterface<T> &, Arg1, Arg2), const Arg1 &arg1, const Arg2 &arg2)
-    : fn(fn), arg1(arg1), arg2(arg2) { }
-
-    QFuture<T> start()
-    {
-        futureInterface.reportStarted();
-        QFuture<T> future = futureInterface.future();
-        QThreadPool::globalInstance()->start(this);
-        return future;
-    }
-
-    void run() override
-    {
-        fn(futureInterface, arg1, arg2);
-        futureInterface.reportFinished();
-    }
-private:
-    QFutureInterface<T> futureInterface;
-    FunctionPointer fn;
-    Arg1 arg1; Arg2 arg2;
-};
-template <typename T,  typename FunctionPointer, typename Class, typename Arg1, typename Arg2>
-class StoredInterfaceMemberFunctionCall2 : public QRunnable
-{
-public:
-    StoredInterfaceMemberFunctionCall2(void (Class::*fn)(QFutureInterface<T> &, Arg1, Arg2), Class *object, const Arg1 &arg1, const Arg2 &arg2)
-    : fn(fn), object(object), arg1(arg1), arg2(arg2) { }
-
-    QFuture<T> start()
-    {
-        futureInterface.reportStarted();
-        QFuture<T> future = futureInterface.future();
-        QThreadPool::globalInstance()->start(this);
-        return future;
-    }
-
-    void run() override
-    {
-        (object->*fn)(futureInterface, arg1, arg2);
-        futureInterface.reportFinished();
-    }
-private:
-    QFutureInterface<T> futureInterface;
-    FunctionPointer fn;
-    Class *object;
-    Arg1 arg1; Arg2 arg2;
-};
-
-template <typename T,  typename FunctionPointer, typename Arg1, typename Arg2, typename Arg3>
-class StoredInterfaceFunctionCall3 : public QRunnable
-{
-public:
-    StoredInterfaceFunctionCall3(void (fn)(QFutureInterface<T> &, Arg1, Arg2, Arg3), const Arg1 &arg1, const Arg2 &arg2, const Arg3 &arg3)
-    : fn(fn), arg1(arg1), arg2(arg2), arg3(arg3) { }
-
-    QFuture<T> start()
-    {
-        futureInterface.reportStarted();
-        QFuture<T> future = futureInterface.future();
-        QThreadPool::globalInstance()->start(this);
-        return future;
-    }
-
-    void run() override
-    {
-        fn(futureInterface, arg1, arg2, arg3);
-        futureInterface.reportFinished();
-    }
-private:
-    QFutureInterface<T> futureInterface;
-    FunctionPointer fn;
-    Arg1 arg1; Arg2 arg2; Arg3 arg3;
-};
-template <typename T,  typename FunctionPointer, typename Class, typename Arg1, typename Arg2, typename Arg3>
-class StoredInterfaceMemberFunctionCall3 : public QRunnable
-{
-public:
-    StoredInterfaceMemberFunctionCall3(void (Class::*fn)(QFutureInterface<T> &, Arg1, Arg2, Arg3), Class *object, const Arg1 &arg1, const Arg2 &arg2, const Arg3 &arg3)
-    : fn(fn), object(object), arg1(arg1), arg2(arg2), arg3(arg3) { }
-
-    QFuture<T> start()
-    {
-        futureInterface.reportStarted();
-        QFuture<T> future = futureInterface.future();
-        QThreadPool::globalInstance()->start(this);
-        return future;
-    }
-
-    void run() override
-    {
-        (object->*fn)(futureInterface, arg1, arg2, arg3);
-        futureInterface.reportFinished();
-    }
-private:
-    QFutureInterface<T> futureInterface;
-    FunctionPointer fn;
-    Class *object;
-    Arg1 arg1; Arg2 arg2; Arg3 arg3;
-};
-
-template <typename T,  typename FunctionPointer, typename Arg1, typename Arg2, typename Arg3, typename Arg4>
-class StoredInterfaceFunctionCall4 : public QRunnable
-{
-public:
-    StoredInterfaceFunctionCall4(void (fn)(QFutureInterface<T> &, Arg1, Arg2, Arg3, Arg4), const Arg1 &arg1, const Arg2 &arg2, const Arg3 &arg3, const Arg4 &arg4)
-    : fn(fn), arg1(arg1), arg2(arg2), arg3(arg3), arg4(arg4) { }
-
-    QFuture<T> start()
-    {
-        futureInterface.reportStarted();
-        QFuture<T> future = futureInterface.future();
-        QThreadPool::globalInstance()->start(this);
-        return future;
-    }
-
-    void run() override
-    {
-        fn(futureInterface, arg1, arg2, arg3, arg4);
-        futureInterface.reportFinished();
-    }
-private:
-    QFutureInterface<T> futureInterface;
-    FunctionPointer fn;
-    Arg1 arg1; Arg2 arg2; Arg3 arg3; Arg4 arg4;
-};
-template <typename T,  typename FunctionPointer, typename Class, typename Arg1, typename Arg2, typename Arg3, typename Arg4>
-class StoredInterfaceMemberFunctionCall4 : public QRunnable
-{
-public:
-    StoredInterfaceMemberFunctionCall4(void (Class::*fn)(QFutureInterface<T> &, Arg1, Arg2, Arg3, Arg4), Class *object, const Arg1 &arg1, const Arg2 &arg2, const Arg3 &arg3, const Arg4 &arg4)
-    : fn(fn), object(object), arg1(arg1), arg2(arg2), arg3(arg3), arg4(arg4) { }
-
-    QFuture<T> start()
-    {
-        futureInterface.reportStarted();
-        QFuture<T> future = futureInterface.future();
-        QThreadPool::globalInstance()->start(this);
-        return future;
-    }
-
-    void run() override
-    {
-        (object->*fn)(futureInterface, arg1, arg2, arg3, arg4);
-        futureInterface.reportFinished();
-    }
-private:
-    QFutureInterface<T> futureInterface;
-    FunctionPointer fn;
-    Class *object;
-    Arg1 arg1; Arg2 arg2; Arg3 arg3; Arg4 arg4;
-};
-
-template <typename T,  typename FunctionPointer, typename Arg1, typename Arg2, typename Arg3, typename Arg4, typename Arg5>
-class StoredInterfaceFunctionCall5 : public QRunnable
-{
-public:
-    StoredInterfaceFunctionCall5(void (fn)(QFutureInterface<T> &, Arg1, Arg2, Arg3, Arg4, Arg5), const Arg1 &arg1, const Arg2 &arg2, const Arg3 &arg3, const Arg4 &arg4, const Arg5 &arg5)
-    : fn(fn), arg1(arg1), arg2(arg2), arg3(arg3), arg4(arg4), arg5(arg5) { }
-
-    QFuture<T> start()
-    {
-        futureInterface.reportStarted();
-        QFuture<T> future = futureInterface.future();
-        QThreadPool::globalInstance()->start(this);
-        return future;
-    }
-
-    void run() override
-    {
-        fn(futureInterface, arg1, arg2, arg3, arg4, arg5);
-        futureInterface.reportFinished();
-    }
-private:
-    QFutureInterface<T> futureInterface;
-    FunctionPointer fn;
-    Arg1 arg1; Arg2 arg2; Arg3 arg3; Arg4 arg4; Arg5 arg5;
-};
-template <typename T,  typename FunctionPointer, typename Class, typename Arg1, typename Arg2, typename Arg3, typename Arg4, typename Arg5>
-class StoredInterfaceMemberFunctionCall5 : public QRunnable
-{
-public:
-    StoredInterfaceMemberFunctionCall5(void (Class::*fn)(QFutureInterface<T> &, Arg1, Arg2, Arg3, Arg4, Arg5), Class *object, const Arg1 &arg1, const Arg2 &arg2, const Arg3 &arg3, const Arg4 &arg4, const Arg5 &arg5)
-    : fn(fn), object(object), arg1(arg1), arg2(arg2), arg3(arg3), arg4(arg4), arg5(arg5) { }
-
-    QFuture<T> start()
-    {
-        futureInterface.reportStarted();
-        QFuture<T> future = futureInterface.future();
-        QThreadPool::globalInstance()->start(this);
-        return future;
-    }
-
-    void run() override
-    {
-        (object->*fn)(futureInterface, arg1, arg2, arg3, arg4, arg5);
-        futureInterface.reportFinished();
-    }
-private:
-    QFutureInterface<T> futureInterface;
-    FunctionPointer fn;
-    Class *object;
-    Arg1 arg1; Arg2 arg2; Arg3 arg3; Arg4 arg4; Arg5 arg5;
-};
-
-template <typename T>
-QFuture<T> run(void (*functionPointer)(QFutureInterface<T> &))
-{
-    return (new StoredInterfaceFunctionCall0<T, void (*)(QFutureInterface<T> &)>(functionPointer))->start();
-}
-template <typename T, typename Arg1>
-QFuture<T> run(void (*functionPointer)(QFutureInterface<T> &, Arg1), const Arg1 &arg1)
-{
-    return (new StoredInterfaceFunctionCall1<T, void (*)(QFutureInterface<T> &, Arg1), Arg1>(functionPointer, arg1))->start();
-}
-template <typename T, typename Arg1, typename Arg2>
-QFuture<T> run(void (*functionPointer)(QFutureInterface<T> &, Arg1, Arg2), const Arg1 &arg1, const Arg2 &arg2)
-{
-    return (new StoredInterfaceFunctionCall2<T, void (*)(QFutureInterface<T> &, Arg1, Arg2), Arg1, Arg2>(functionPointer, arg1, arg2))->start();
-}
-template <typename T, typename Arg1, typename Arg2, typename Arg3>
-QFuture<T> run(void (*functionPointer)(QFutureInterface<T> &, Arg1, Arg2, Arg3), const Arg1 &arg1, const Arg2 &arg2, const Arg3 &arg3)
-{
-    return (new StoredInterfaceFunctionCall3<T, void (*)(QFutureInterface<T> &, Arg1, Arg2, Arg3), Arg1, Arg2, Arg3>(functionPointer, arg1, arg2, arg3))->start();
-}
-template <typename T, typename Arg1, typename Arg2, typename Arg3, typename Arg4>
-QFuture<T> run(void (*functionPointer)(QFutureInterface<T> &, Arg1, Arg2, Arg3, Arg4), const Arg1 &arg1, const Arg2 &arg2, const Arg3 &arg3, const Arg4 &arg4)
-{
-    return (new StoredInterfaceFunctionCall4<T, void (*)(QFutureInterface<T> &, Arg1, Arg2, Arg3, Arg4), Arg1, Arg2, Arg3, Arg4>(functionPointer, arg1, arg2, arg3, arg4))->start();
-}
-template <typename T, typename Arg1, typename Arg2, typename Arg3, typename Arg4, typename Arg5>
-QFuture<T> run(void (*functionPointer)(QFutureInterface<T> &, Arg1, Arg2, Arg3, Arg4, Arg5), const Arg1 &arg1, const Arg2 &arg2, const Arg3 &arg3, const Arg4 &arg4, const Arg5 &arg5)
-{
-    return (new StoredInterfaceFunctionCall5<T, void (*)(QFutureInterface<T> &, Arg1, Arg2, Arg3, Arg4, Arg5), Arg1, Arg2, Arg3, Arg4, Arg5>(functionPointer, arg1, arg2, arg3, arg4, arg5))->start();
+    return (new StoredInterfaceFunctionCall<T, void (*)(QFutureInterface<T> &, Args...), Args...>(functionPointer, args...))->start();
 }
 
-template <typename Class, typename T>
-QFuture<T> run(void (Class::*fn)(QFutureInterface<T> &), Class *object)
+template <typename Class, typename T, typename... Args>
+QFuture<T> run(void (Class::*fn)(QFutureInterface<T> &, Args...), Class *object, Args... args)
 {
-    return (new StoredInterfaceMemberFunctionCall0<T, void (Class::*)(QFutureInterface<T> &), Class>(fn, object))->start();
+    return (new StoredInterfaceMemberFunctionCall<T, void (Class::*)(QFutureInterface<T> &, Args...), Class, Args...>(fn, object, args...))->start();
 }
 
-template <typename Class, typename T, typename Arg1>
-QFuture<T> run(void (Class::*fn)(QFutureInterface<T> &, Arg1), Class *object, Arg1 arg1)
-{
-    return (new StoredInterfaceMemberFunctionCall1<T, void (Class::*)(QFutureInterface<T> &, Arg1), Class, Arg1>(fn, object, arg1))->start();
-}
-
-template <typename Class, typename T, typename Arg1, typename Arg2>
-QFuture<T> run(void (Class::*fn)(QFutureInterface<T> &, Arg1, Arg2), Class *object, const Arg1 &arg1, const Arg2 &arg2)
-{
-    return (new StoredInterfaceMemberFunctionCall2<T, void (Class::*)(QFutureInterface<T> &, Arg1, Arg2), Class, Arg1, Arg2>(fn, object, arg1, arg2))->start();
-}
-
-template <typename Class, typename T, typename Arg1, typename Arg2, typename Arg3>
-QFuture<T> run(void (Class::*fn)(QFutureInterface<T> &, Arg1, Arg2, Arg3), Class *object, const Arg1 &arg1, const Arg2 &arg2, const Arg3 &arg3)
-{
-    return (new StoredInterfaceMemberFunctionCall3<T, void (Class::*)(QFutureInterface<T> &, Arg1, Arg2, Arg3), Class, Arg1, Arg2, Arg3>(fn, object, arg1, arg2, arg3))->start();
-}
-
-template <typename Class, typename T, typename Arg1, typename Arg2, typename Arg3, typename Arg4>
-QFuture<T> run(void (Class::*fn)(QFutureInterface<T> &, Arg1, Arg2, Arg3, Arg4), Class *object, const Arg1 &arg1, const Arg2 &arg2, const Arg3 &arg3, const Arg4 &arg4)
-{
-    return (new StoredInterfaceMemberFunctionCall4<T, void (Class::*)(QFutureInterface<T> &, Arg1, Arg2, Arg3, Arg4), Class, Arg1, Arg2, Arg3, Arg4>(fn, object, arg1, arg2, arg3, arg4))->start();
-}
-
-template <typename Class, typename T, typename Arg1, typename Arg2, typename Arg3, typename Arg4, typename Arg5>
-QFuture<T> run(void (Class::*fn)(QFutureInterface<T> &, Arg1, Arg2, Arg3, Arg4, Arg5), Class *object, const Arg1 &arg1, const Arg2 &arg2, const Arg3 &arg3, const Arg4 &arg4, const Arg5 &arg5)
-{
-    return (new StoredInterfaceMemberFunctionCall5<T, void (Class::*)(QFutureInterface<T> &, Arg1, Arg2, Arg3, Arg4, Arg5), Class, Arg1, Arg2, Arg3, Arg4, Arg5>(fn, object, arg1, arg2, arg3, arg4, arg5))->start();
-}
 } // namespace QtConcurrent
 
 QT_END_NAMESPACE
