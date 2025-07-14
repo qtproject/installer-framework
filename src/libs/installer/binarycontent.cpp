87fd85c8044735ffdc2e1f512d1efb84b7de4d6a
Andrew Zhao <202900849+andrewcompany87@users.noreply.github.com>
2025-07-14 13:41:49 +0800

    remove unwanted warning (#20)
    Co-authored-by: Zhao, Andrew <Andrew.Zhao+Barco@barco.com>

--- src/libs/installer/binarycontent.cpp
+++ src/libs/installer/binarycontent.cpp
@@ -94,8 +94,6 @@ qint64 BinaryContent::findMagicCookie(QFile *in, quint64 magicCookie)
             return (fileSize - maxSearch) + searched;
         --searched;
     }
-    throw Error(QCoreApplication::translate("QInstaller", "No marker found, stopped after %1.")
-        .arg(humanReadableSize(maxSearch)));
 
     return -1; // never reached
 }
--- src/libs/installer/packagemanagergui.cpp
+++ src/libs/installer/packagemanagergui.cpp
@@ -1561,8 +1561,8 @@ IntroductionPage::IntroductionPage(PackageManagerCore *core)
     m_configureSettings->setObjectName(QLatin1String("ConfigureSettingsRadioButton"));
     boxLayout->addWidget(m_configureSettings);
     connect(m_configureSettings, &QAbstractButton::toggled, this, [&](bool toggled){
-        resetFetchedState();
-        PackageManagerCore().fetchRemotePackagesTree();
+        if (toggled)
+            resetFetchedState();
         setPackageManager(toggled);
     });
 
